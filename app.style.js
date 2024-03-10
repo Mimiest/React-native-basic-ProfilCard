c1d5f74765adc43707f639cc544b287d5935f445
Meryem <errajouanym@gmail.com>
2024-03-10 22:09:16 +0000

    views

---
+++ app.style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+  },
+  box1: {
+    backgroundColor: "green",
+  },
+  box2: {
+    backgroundColor: "yellow",
+    alignSelf: "flex-start",
+  },
+  box3: {
+    backgroundColor: "magenta",
+  },
+});
---
+++ components/Profil/Profil.jsx
@@ -0,0 +1,10 @@
+import { Text, TouchableOpacity } from "react-native";
+
+export function Profil() {
+  return (
+    <>
+      <text>there</text>
+      <TouchableOpacity>Click here</TouchableOpacity>
+    </>
+  );
+}
