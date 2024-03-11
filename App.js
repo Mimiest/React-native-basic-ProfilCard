d69e410bce8201a066ee74e2feed6b4f99834770
Meryem <errajouanym@gmail.com>
2024-03-11 19:41:00 +0000

    ProfilCard

--- App.js
+++ App.js
@@ -1,23 +1,6 @@
-import { Text, View } from "react-native";
-import { Profil } from "./components/Profil/Profil";
-import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import s from "./app.style";
+import ProfilCard from "./components/ProfilCard/ProfilCard";
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "space-around",
-          alignItems: "center",
-        }}
-      >
-        <view style={s.box1}></view>
-        <view style={s.box2}></view>
-        <view style={s.box3}></view>
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
+  return <ProfilCard />;
 }
